0c83ea23c6f19ee5b457d85f4bb49057e0eb4925
adamspittler <spittler@gmail.com>
2016-04-17 20:49:20 -0400

    adds Pseudocode and tries to make the code more clear

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,4 +1,4 @@
-
+# the encrypt method pushes all the letters in a string up by one.
 def encrypt(word)
 	x = 0
 	while x < word.length
@@ -11,7 +11,7 @@ def encrypt(word)
 	end
 	word
 end
-
+# I added secret encrypt and decrypt methods that are a little more complicated, they are only accessed if the user knows their name.
 def encrypt2(word)
 	x = 0
 	while x < word.length
@@ -28,7 +28,7 @@ def encrypt2(word)
 	end
 	word.reverse
 end
-
+# the decrypt method pulls all the letters in a string back by one.
 def decrypt(word)
 	x = 0
 	str = "abcdefghijklmnopqrstuvwxyz"
@@ -49,27 +49,28 @@ def decrypt2(word)
 	end
 	word.reverse
 end
-
-puts "would you like to encrypt or decrypt a password"
-password = gets.chomp
-if password == "encrypt" 
-puts "enter now"	
-elsif password == "decrypt" 
-puts "enter now"
-elsif password == "ensecret" 
-puts "enter now"
-elsif password == "desecret"
-puts "enter now"
-else puts "go away"
+# Driver code
+#decrypt(encrypt("swordfish"))
+puts "would you like to encrypt or decrypt a encryption_selection"
+encryption_selection = gets.chomp
+if encryption_selection == "encrypt" 
+	puts "enter now (please letters only and no spaces)"	
+	elsif encryption_selection == "decrypt" 
+	puts "enter now"
+	elsif encryption_selection == "ensecret" 
+	puts "enter now"
+	elsif encryption_selection == "desecret"
+	puts "enter now"
+	else puts "go away"
 end
 word = gets.chomp
-if password == "encrypt" 
-puts encrypt(word)	
-elsif password == "decrypt" 
-puts decrypt(word)
-elsif password == "ensecret" 
-puts encrypt2(word)
-elsif password == "desecret"
-puts decrypt2(word)
-else puts "really go away"
+if encryption_selection == "encrypt" 
+	puts encrypt(word)	
+	elsif encryption_selection == "decrypt" 
+	puts decrypt(word)
+	elsif encryption_selection == "ensecret" 
+	puts encrypt2(word)
+	elsif encryption_selection == "desecret"
+	puts decrypt2(word)
+	else puts "really go away"
 end
