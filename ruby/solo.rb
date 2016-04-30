eb147b7ff18815d2ef992d66fb5168987cc001f8
adamspittler <spittler@gmail.com>
2016-04-30 14:56:05 -0400

    Adds sleep the night method, about method and updates the driver code.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -17,18 +17,25 @@ class Mosquito
 	attr_accessor :name
 
 	def initialize(name, age)
-		puts "A mosquito named #{name} is buzzing around your room"
+		puts "BZZZZZZZZZ"
 		@name = name
 		@age = age
 		@has_sucked_blood = false
 	end
 
 	def about
-		puts "a mosquito named #{@name} is buzzing around your room."
+		puts "a #{@age} day old mosquito named #{@name} is buzzing around your room."
 	end
 
 	def sleep_the_night
 		@age += 1
+		body_part = ["head", "neck", "chest", "left arm", "right arm", "stomach", "back", "left leg", "right leg", "ear", "nose", "forehead", "left foot", "right leg"]
+		random_number = rand(1..10)
+		if random_number > 5 
+			puts "#{@name} has bit you and now you have a welt on your #{body_part.sample}"
+			@has_sucked_blood = true
+			else puts "#{@name} didn't bite you tonight"
+		end
 	end
 
 
@@ -37,12 +44,18 @@ end
 #driver code
 
 mosquitos = []
+mosquito_name = ["Buzzy", "Sucky", "Stabby", "Zeke", "blood Sucka", "Mr. Mosquito", "Mary Laria",]
 3.times do |making_mosquitos|
 
- 	mosquitos << Mosquito.new("buzzy", 0)
+ 	mosquitos << Mosquito.new(mosquito_name.sample, rand(0..5))
  	end
 
 mosquitos.each do |mosquito|
   mosquito.about
+ end
+  
+
+mosquitos.each do |mosquito|
+	mosquito.sleep_the_night
   
 end
