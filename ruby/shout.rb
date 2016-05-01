9fbfa863123883fc01a6a760281baac2ca87003a
adamspittler <spittler@gmail.com>
2016-05-01 13:31:47 -0400

    Adds mixins of Shout variable and creates two classes that can use it.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,19 +1,48 @@
-#Defining module
+##Defining module
+#module Shout
+#
+#  def self.yell_angrily(words)
+#    words + "!!!" + " :("
+#  end
+#
+#
+#def self.yelling_happily(words)
+#    words + "$$$" + " :)"
+#  end
+#
+#end
+#
+##Driver code
+#
+#p Shout.yelling_happily("words of happiness")
+#
+#p Shout.yell_angrily("words of anger")
+
 module Shout
 
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    words.upcase + "!!!!!!" + " :("
   end
 
 
-def self.yelling_happily(words)
-    words + "$$$" + " :)"
+def yell_happily(words)
+    words.upcase + "$$$" + " :)  :)"
   end
 
 end
 
-#Driver code
+class Gym_teacher
+  include Shout
+end
 
-p Shout.yelling_happily("words of happiness")
+class Drunk_person
+  include Shout
+end
 
-p Shout.yell_angrily("words of anger")
+larry = Gym_teacher.new
+p larry.yell_angrily("there's no crying in baseball")
+p larry.yell_happily("we win, we win")
+
+vikki = Drunk_person.new
+p vikki.yell_happily("i'm having the best time, let's go to another bar")
+p vikki.yell_angrily("what do you mean i'm too drunk to get in")
