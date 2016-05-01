2de6265dc0d8643399990301993f5ba63e5357de
adamspittler <spittler@gmail.com>
2016-04-30 21:52:40 -0400

    Add more psurdo coding and the beginings of a user interface.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -3,12 +3,23 @@
 	# Attributs are:
 		# Name (these mosquitos are anthromorphic)
 		# Age in days.
-		# "Has sucked your blood" boolian value. 
+		# "Has sucked your blood" boolian value. determines if the mosquito is full of blood.
+		# "Has been killed" boolean value. determies if the mosquito is alive or dead.
 
 	# Methods are:
-		# initialize: creates between 1 and 10 instances of Mosquito
-		# Swat: percentage chance of success. if sucessfull kills (deletes) Mosquito instance, if mosquito has sucked your blood, it leaves a red spot on your wall.
+		# initialize: creates between 2 and 10 instances of Mosquito.
+		# About: gives the stat of all the mosquitos.
+		# Swat: 60 percentage chance of success. if sucessfull kills (deletes) Mosquito instance, if mosquito has sucked your blood, it leaves a red spot on your wall.
 		# Sleep the night: ages each mosquito instance by one day. each Mosquito instance has percentage chance of sucking your blood.
+
+	# User interface:
+		# Ask user if they want to open the window.
+		# If they do it allows the mosquito s in.
+		# Tell the user how many mosquitos are flying around and their names.
+		# Asks the user if they want to try to kill the mosquitos before bed, if they do runs swat method, if not runs sleep the night method.
+		# gives the user two chances to try to kill the mosquitos before they are too tired and go to bed.
+		# After the sleep the night is run gives the user to open the window the next day and repeats the rest of the structure until all the mosquitos are gone.
+
 
 class Mosquito
 
@@ -35,7 +46,7 @@ class Mosquito
 			puts "You killed #{@name}, and now you have a gross blood stain on your wall" 
 			@has_been_killed = true
 			elsif random_number > 4 && @has_sucked_blood == false
-			puts "Good for you, you killed #{@name} the mosquito before it bit you"
+			puts "Good for you, you killed #{@name} before it bit you"
 			@has_been_killed = true
 			else puts " Too slow!! you missed #{@name}"
 		end
@@ -61,30 +72,62 @@ end
 #driver code
 
 mosquitos = []
-mosquito_name = ["Buzzy", "Sucky", "Stabby", "Zeke", "blood Sucka", "Mr. Bite You", "Mary Laria",]
-3.times do |making_mosquitos|
+mosquito_name = ["Buzzy", "Sucky", "Stabby", "Zeke", "Blood Sucka", "Mr. Bite You", "Mary Laria", "Bill the Butcher", "Sting", "Little Evil", "Tu Fang", "Succubus", "Buzz Aldrin", "Christopher Columbuzz", "Juicy Lucy", "Typhoid Mary", "Blood Bourne", "Smudge Judy"]
+puts "Do you want to open the window this afternoon?"
+window_open = gets.chomp
+until window_open == "yes" or window_open == "no"
+	puts "Please answer yes or no."
+	window_open = gets.chomp
+end	
+	if window_open == "yes" 
+			rand(2..10).times do |making_mosquitos|
+			mosquitos << Mosquito.new(mosquito_name.sample, rand(0..5))
+			end
+			puts "you let some mosquitos in"
+ 			puts ""
+ 			mosquitos.each do |mosquito|
+				mosquito.about
+			end
 
- 	mosquitos << Mosquito.new(mosquito_name.sample, rand(0..5))
- 	end
 
-mosquitos.each do |mosquito|
-  mosquito.about
+
+
+
+
+
+ 		else
+ 			puts "it's pretty stuffy in here, but suit yorself."
+	end
 end
-  
 
-mosquitos.each do |mosquito|
-	mosquito.sleep_the_night
-  
-end
 
-mosquitos.each do |mosquito|
-	mosquito.swat
 
-end
 
-mosquitos.each do |mosquito|
-  mosquito.about
-end
 
-#p mosquitos
-end
+
+#rand(2..10).times do |making_mosquitos|
+#
+# 	mosquitos << Mosquito.new(mosquito_name.sample, rand(0..5))
+# 	end
+#
+#mosquitos.each do |mosquito|
+#  mosquito.about
+#end
+#  
+#
+#mosquitos.each do |mosquito|
+#	mosquito.sleep_the_night
+#  
+#end
+#
+#mosquitos.each do |mosquito|
+#	mosquito.swat
+#
+#end
+#
+#mosquitos.each do |mosquito|
+#  mosquito.about
+#end
+#
+##p mosquitos
+#end
