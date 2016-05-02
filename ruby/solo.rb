9443b48c7ba0943a3c0e745eb66ef45b1b7dd02a
adamspittler <spittler@gmail.com>
2016-05-01 20:46:48 -0400

    Adds new user interface and updates psuedo code. also Adds get info method and stats method.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -7,18 +7,23 @@
 		# "Has been killed" boolean value. determies if the mosquito is alive or dead.
 
 	# Methods are:
-		# initialize: creates between 2 and 10 instances of Mosquito.
-		# About: gives the stat of all the mosquitos.
-		# Swat: 60 percentage chance of success. if sucessfull kills (deletes) Mosquito instance, if mosquito has sucked your blood, it leaves a red spot on your wall.
+		# Initialize: creates between 2 and 10 instances of Mosquito.
+		# Get info: allows the user to give names and ages to their mosquitos.
+		# About: gives which mosquitos are still flying around.
+		# Swat: percentage chance of success. if sucessfull kills (deletes) Mosquito instance, if mosquito has sucked your blood, it leaves a red spot on your wall.
 		# Sleep the night: ages each mosquito instance by one day. each Mosquito instance has percentage chance of sucking your blood.
+		# Stats: Gives a full update on what became of each mosquito.
 
 	# User interface:
+		# Prompt the user to make a number of mosquitos, and then give them names and ages.
 		# Ask user if they want to open the window.
-		# If they do it allows the mosquito s in.
+		# If they do it allows the mosquitos in.
 		# Tell the user how many mosquitos are flying around and their names.
-		# Asks the user if they want to try to kill the mosquitos before bed, if they do runs swat method, if not runs sleep the night method.
-		# gives the user two chances to try to kill the mosquitos before they are too tired and go to bed.
-		# After the sleep the night is run gives the user to open the window the next day and repeats the rest of the structure until all the mosquitos are gone.
+		# Asks the user if they want to try to kill the mosquitos before bed, if they do runs swat method.
+		# Tell the user how many mosquitos they killed.
+		# Runs sleep the night method, allowing the mosquitos to try to bite.
+		# Gives the user another chance to kill the mosquitos.
+		# Reports on the current status of all mosquitos.
 
 
 class Mosquito
@@ -35,9 +40,23 @@ class Mosquito
 		@has_been_killed = false
 	end
 
+	def get_info
+		puts "Whats this mosquito's name?"
+		@name = gets.chomp
+		puts "How many days old is this mosquito?"
+		days_old = gets.chomp.to_i
+		until days_old.class == Fixnum
+			puts "please enter a number"
+			days_old = gets.chomp.to_i
+		end
+		@age = days_old
+	end
+
 	def about
 		if @has_been_killed == false
 		puts "a #{@age} day old mosquito named #{@name} is buzzing around your room."
+		end
+		
 	end
 
 	def swat
@@ -46,10 +65,11 @@ class Mosquito
 			puts "You killed #{@name}, and now you have a gross blood stain on your wall" 
 			@has_been_killed = true
 			elsif random_number > 4 && @has_sucked_blood == false
-			puts "Good for you, you killed #{@name} before it bit you"
+			puts "Good for you, you killed #{@name} before it could bite you"
 			@has_been_killed = true
-			else puts " Too slow!! you missed #{@name}"
+			else puts "Too slow!! you missed #{@name}"
 		end
+		
 	end
 
 	def sleep_the_night
@@ -60,10 +80,23 @@ class Mosquito
 			puts "#{@name} has bit you and now you have a welt on your #{body_part.sample}"
 			@has_sucked_blood = true
 			elsif  random_number > 5 && @has_been_killed == true
-			puts "#{@name} didn't bite you because it's dead"
+			#puts "#{@name} didn't bite you because it's dead"
 			else	
 			puts "#{@name} didn't bite you tonight, but continues to buzz around."
 		end
+		
+	end
+
+	def stats
+		if @has_been_killed == true && @has_sucked_blood == false
+			puts "#{@name}, #{@age}days old is now stuck to your wall, and never got the pleasure of biting you."
+			elsif @has_been_killed == true && @has_sucked_blood == true
+				puts "#{@name}, #{@age}days old is now a blood stain on your wall"
+			elsif @has_been_killed == false && @has_sucked_blood == true
+				puts "#{@name}, #{@age}days old is a happy and full mosquito"
+			else puts "#{@name}, #{@age}days old never bit you but avoided smashing"
+		end
+		
 	end
 
 
@@ -73,88 +106,130 @@ end
 
 mosquitos = []
 mosquito_name = ["Buzzy", "Sucky", "Stabby", "Zeke", "Blood Sucka", "Mr. Bite You", "Mary Laria", "Bill the Butcher", "Sting", "Little Evil", "Tu Fang", "Succubus", "Buzz Aldrin", "Christopher Columbuzz", "Juicy Lucy", "Typhoid Mary", "Blood Bourne", "Smudge Judy"]
-puts "Do you want to open the window this evening?"
+
+puts "How many mosquitos would you like to make?"
+mosquito_number = gets.chomp.to_i
+until mosquito_number.class == Fixnum
+	puts "please enter a number"
+	mosquito_number = gets.chomp.to_i
+end
+mosquito_number.times do |making_mosquitos|
+
+ 	mosquitos << Mosquito.new("N/A", "N/A")
+end
+
+puts "please enter a name and an age for each mosquito."
+
+mosquitos.each do |mosquito|
+	mosquito.get_info
+end
+
+puts ""
+
+puts "would you like to leave the window open?"
 window_open = gets.chomp
 until window_open == "yes" or window_open == "no"
-	puts "Please answer yes or no."
+	puts "please answer yes or no."
 	window_open = gets.chomp
-end	
-	if window_open == "yes" 
-			rand(2..10).times do |making_mosquitos|
-			mosquitos << Mosquito.new(mosquito_name.sample, rand(0..5))
-			end
-			puts "you let some mosquitos in"
- 			puts ""
- 			mosquitos.each do |mosquito|
-				mosquito.about
-			end
+end
+if window_open == "yes"
+	rand(2..10).times do |making_mosquitos|
+	
+	 	mosquitos << Mosquito.new(mosquito_name.sample, rand(0..5))
+	 	end
+	else
+	puts "Probably a good idea to leave it closed."
+end
 
+puts ""
 
-			puts "do you want to try to kill the mosquitos, or go to bed"
-			kill_or_bed = gets.chomp
-			
-			until kill_or_bed == "kill the mosquitos" or kill_or_bed == "go to bed"
-				puts "Please answer kill the mosquitos, or go to bed"
-				kill_or_bed = gets.chomp
-			end	
+puts "would you like to know how many are in here now?"
+how_many = gets.chomp
+until how_many == "yes" or how_many == "no"
+	puts "please answer yes or no."
+	how_many = gets.chomp
+end
 
-			if kill_or_bed == "kill the mosquitos"
-					mosquitos.each do |mosquito|
-						mosquito.swat
-					end
-					puts ""
-					mosquitos.each do |mosquito|
-						mosquito.about
-					end
-					puts "Now you're tired and you need to sleep."
-					mosquitos.each do |mosquito|
-						mosquito.sleep_the_night
-					end
-				
-				else 
-					mosquitos.each do |mosquito|
-						mosquito.sleep_the_night
-					 
-					end
-			end
+puts ""
 
-			puts "Do you want to open the window again the next evening?"
-			window_open = gets.chomp
-			until window_open == "yes" or window_open == "no"
-				puts "Please answer yes or no."
-				window_open = gets.chomp
-			end	
-			if window_open == "yes" 
-						rand(2..6).times do |making_mosquitos|
-						mosquitos << Mosquito.new(mosquito_name.sample, rand(0..5))
-						end
-						puts "you let some mosquitos in"
-			 			puts ""
-			 			mosquitos.each do |mosquito|
-							mosquito.about
-						end
-				else puts "well at least you didn't let any more in."
-					mosquitos.length.to_i = index
-					p index
-			end		
+if how_many == "yes"
+	p "there are #{mosquitos.length}"
+	else
+		puts "I guess it's better not to know."
+end
 
+puts ""
 
+puts "would you like to try to kill them?"
+kill_try = gets.chomp
+until kill_try == "yes" or kill_try == "no"
+	puts "please answer yes or no."
+	kill_try = gets.chomp
+end
+if kill_try == "yes"
+	mosquitos.each do |mosquito|
+	mosquito.swat
+end
+	else
+		puts "Suit yourself."
+end
 
+puts ""
 
-
-
-
-
-
- 		else
- 			puts "it's pretty stuffy in here, but suit yorself."
-	end
+puts "would you like to know how many are left?"
+status = gets.chomp
+until status == "yes" or status == "no"
+	puts "please answer yes or no."
+	status = gets.chomp
+end
+if kill_try == "yes"
+	mosquitos.each do |mosquito|
+	mosquito.about
+end
+	else
+		puts "Suit yourself."
 end
 
 
+puts ""	
+puts "ok time ot go to bed"
+puts ""
+mosquitos.each do |mosquito|
+	mosquito.sleep_the_night
+  
+end	
 
+puts ""
 
+puts "would you like to try to kill them again?"
+kill_try = gets.chomp
+until kill_try == "yes" or kill_try == "no"
+	puts "please answer yes or no."
+	kill_try = gets.chomp
+end
+if kill_try == "yes"
+	mosquitos.each do |mosquito|
+	mosquito.swat
+end
+	else
+		puts "Suit yourself."
+end
 
+puts ""
+
+puts "would you like to know the status of the mosquitos?"
+know_stats = gets.chomp 
+until know_stats == "yes" or know_stats == "no"
+	puts "please answer yes or no."
+	know_stats = gets.chomp
+end
+if know_stats == "yes"
+	mosquitos.each do |mosquito|
+	mosquito.stats
+end
+
+	else puts "ok"
+end
 
 #rand(2..10).times do |making_mosquitos|
 #
