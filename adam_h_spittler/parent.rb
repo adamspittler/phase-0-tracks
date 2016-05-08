393dbaf6591493c0fd48c5f76e946b0ae29410e0
adamspittler <spittler@gmail.com>
2016-05-08 15:30:32 -0400

    Adds hear_request instance method.

--- adam_h_spittler/parent.rb
+++ adam_h_spittler/parent.rb
@@ -8,6 +8,7 @@ class Parent
 	def initialize(name, sleep_amount)
 	@name = name
 	@sleep_amount = sleep_amount
+	@past_requests = {}
 	end	
 
 	#Create greet child class method. returns string.
@@ -15,9 +16,32 @@ class Parent
 		"*yaaaawn* I'm up, I'm up. #{@name} is up. What's the matter?"
 	end
 
+	#Create method that takes a request.
+	#Check the word count of the request against sleep amount.
+	#Returns true if sleep amount greater or equal to word count.
+	def hear_request(request)
+		
+		requested_word_count = request.split(' ').length
+		if @sleep_amount >= requested_word_count
+			@past_requests[:request] = true
+			p "Okay, since you said #{request} so nicely."
+		else @past_requests[:request] = false
+			p "Not today, sweetie."
+		end
+
+
+		
+
+		
+	end
+
+
+p @past_requests
 
 
 end
  
 parent = Parent.new("Debbie", 4)
 puts parent.greet_child
+parent.hear_request("I want a toy")
+parent.hear_request("I want a toy please please please")
