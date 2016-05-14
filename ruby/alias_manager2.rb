08e0e57aea604203e331a4938f7b232155b389e8
adamspittler <spittler@gmail.com>
2016-05-14 14:23:53 -0400

    Add release 2, data structure.

--- ruby/alias_manager2.rb
+++ ruby/alias_manager2.rb
@@ -1,13 +1,14 @@
+# METHODS:
 # Method: takes a string(user input). 
 # Down cases the string.
-# splits the string into individual single character strings and adds them to an array.
-# #returns array
+# Splits the string into individual single character strings and adds them to an array.
+# Returns array
 def splitter(input)
  array = input.downcase.split('')
 end
  
 # Method: takes array.
-# contains an array of all vowels.
+# Contains an array of all vowels.
 # Iterates through the inputted array using map! 
 # If character matches a vowel:
 # Contains a second array of vowels that includes a second "a" at the end"
@@ -42,12 +43,12 @@ def consonant_rep(array_of_char)
 end
 
 # Method: takes array.
-# turns the array into a single string
-# splits the string into separate 'word' strings.
+# Turns the array into a single string
+# Splits the string into separate 'word' strings.
 # Reverses order the word strings.
 # Capitalizes words.
 # Combines words to a string.
-# Returns string
+# Returns string.
 def recombiner (array_of_char)
   new_name = array_of_char.join.split(' ').reverse
   new_name.map! do | words |
@@ -55,14 +56,40 @@ def recombiner (array_of_char)
   end
   new_name.join(' ')
 end
-#p recombiner(consonant_rep(vowel_rep(splitter("adam spittler"))))
 
-#USER INTERFACE
+# Method: just like recombiner but takes string and does not reverse the order.
+# Returns string.
+
+def capitalizer (string)
+  new_name = string.downcase.split
+  new_name.map! do | words |
+    words.capitalize
+  end
+  new_name.join(' ')
+end
+
+# Hash to store real and code names.
+names = {} 
+
+# USER INTERFACE
+# Prompts use for name.
+# if input is not "quit" it capitalizes and adds it to names hash as key and adds generated fake name as value.
+# Runs loop until user types quit.
+# Upon quitting, iterates through the names hash and prints the contents as a string.
 puts "Enter name or type 'quit':"
 name = gets.chomp
+if name != "quit" 
+  names[(capitalizer(name))] = recombiner(consonant_rep(vowel_rep(splitter(name))))
+end
 until name == 'quit'
   puts "Fake name is: #{recombiner(consonant_rep(vowel_rep(splitter(name))))}." 
   puts "Enter name or type 'quit':"
   name = gets.chomp
+  if name != "quit" 
+    names[(capitalizer(name))] = recombiner(consonant_rep(vowel_rep(splitter(name))))
+  end
 end
 
+names.each do | name, fake_name |
+    puts "#{fake_name} is actually #{name}."
+end
