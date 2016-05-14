15bde9b51081e69423a46b9c8591c37235f2aa8e
adamspittler <spittler@gmail.com>
2016-05-14 10:42:53 -0400

    Psuedocode release 1.

---
+++ ruby/alias_manager2.rb
@@ -0,0 +1,36 @@
+# Method: takes a string(user input). 
+# Down cases the string.
+# Iterates through the string to add to a hash.
+# Returns hash of each character and space as key with unassigned values.
+
+# Method: takes hash with blank values.
+# Iterates through and adds values of "vowel", "consonant" or "other" to the hash keys.
+# Returns hash.
+
+# Method: takes hash.
+# contains an array of vowels.
+# Iterates through and and if a key has the value "vowel":
+# checks to see what the index is of the matching vowel in the array.
+# Replaces vowel with next vowel by index.
+# Except "u" automatically becomes "a".
+# Returns hash.
+
+# Method: takes hash.
+# contains an array of consonants.
+# Iterates through and and if a key has the value "consonant":
+# checks to see what the index is of the matching consonant in the array.
+# Replaces consonant with next consonant by index.
+# Except "z" automatically becomes "b".
+# Returns hash.
+
+# Method: takes hash.
+# Iterates through hash and adds the values to a string.
+# Returns string.
+
+# Method: takes string.
+# splits the string into separate "word' strings.
+# Reverses order the word strings.
+# Capitalizes words.
+# Combines words to a string.
+# Returns string
+
