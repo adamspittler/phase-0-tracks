a8e12adf7a59c7af0f1c3f7f4d8de1da91861c84
adamspittler <spittler@gmail.com>
2016-05-17 12:31:09 -0400

    Add release 3

--- ruby/santa2.rb
+++ ruby/santa2.rb
@@ -6,6 +6,31 @@ class Santa
     @age = 0
     puts "Initializing Santa instance ..."
   end
+  def gender
+    @gender
+  end
+  def age
+    @age 
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking << reindeer
+  end
+
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
@@ -18,10 +43,19 @@ end
 #santa.speak
 #santa.eat_milk_and_cookies("oreo")
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "none"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "klingon"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[rand(example_genders.length)], example_ethnicities[rand(example_ethnicities.length)])
-end
-p santas
+#santas = []
+#example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "none"]
+#example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "klingon"]
+#example_genders.length.times do |i|
+#  santas << Santa.new(example_genders.sample, example_ethnicities.sample)
+#end
+#p santas
+
+santa = Santa.new("male", "pink")
+
+p santa.get_mad_at("Dasher")
+p santa.celebrate_birthday
+p santa.celebrate_birthday
+p santa.gender=("female")
+p santa.age
+p santa.gender
