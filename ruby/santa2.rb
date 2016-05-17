3039e0d2081f29d1f39cc56c6aaa8650a03264d4
adamspittler <spittler@gmail.com>
2016-05-17 11:58:48 -0400

    Add release 1

--- ruby/santa2.rb
+++ ruby/santa2.rb
@@ -1,5 +1,9 @@
 class Santa
-  def initialize
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
     puts "Initializing Santa instance ..."
   end
   def speak
@@ -10,6 +14,14 @@ class Santa
   end
 
 end
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("oreo")
+#santa = Santa.new
+#santa.speak
+#santa.eat_milk_and_cookies("oreo")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "none"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "klingon"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[rand(example_genders.length)], example_ethnicities[rand(example_ethnicities.length)])
+end
+p santas
