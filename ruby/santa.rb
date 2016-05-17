a6e0ff7795fb9f42ac2237dc8636f741645d4b9a
adamspittler <spittler@gmail.com>
2016-05-17 13:17:45 -0400

    Fixed merge conflict

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,19 +1,14 @@
 class Santa
 
-<<<<<<< HEAD
 	#attr reader and attr accessor shortcuts.
 	attr_reader :age, :ethnicity
 	attr_accessor :gender	
 
 	def initialize(gender, ethnicity, age)
-=======
-	def initialize(gender, ethnicity)
->>>>>>> master
 		puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-<<<<<<< HEAD
 		@age = age
 	end
 
@@ -32,12 +27,6 @@ class Santa
 	#end
 
 	# methods
-=======
-		@age = 0
-
-	end
-
->>>>>>> master
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -49,7 +38,6 @@ class Santa
 	def about
 		puts "Gender is #{@gender}"
 		puts "Ethnicity is #{@ethnicity}"
-<<<<<<< HEAD
 		puts "age is #{@age}"
 		#puts "Reindeer ranking is #{@reindeer_ranking}"
 	end
@@ -71,7 +59,7 @@ end
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "prefer not to say"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Brazilian", "Klingon", "afro-czech", "caucazoidal", "lizard overlord", "Saudi-Canadian", "white walker", "Titan", "Jawa", "mer-person", "floridian", "ninja trutle", "trogladite", "sasquatch"]
-100.times do |making_santas|
+50.times do |making_santas|
 
   santas << Santa.new(example_genders.sample, example_ethnicities.sample,rand(0..140) )
 end
@@ -80,26 +68,3 @@ santas.each do |santa|
   santa.about
   santa.speak
 end
-=======
-		#puts "Reindeer ranking is #{@reindeer_ranking}"
-	end
-
-
-	
-end
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
-
-
-#santa.speak
-#
-#santa.eat_milk_and_cookies("plain cookie")
-#
-santa.about
->>>>>>> master
