08e7fbbd4f294bb11237e0741e21f1814cdd276b
adamspittler <spittler@gmail.com>
2016-05-19 11:42:55 -0400

    Add release 2 and 3.

--- ruby/shout2.rb
+++ ruby/shout2.rb
@@ -1,12 +1,36 @@
+#module Shout2
+#  def self.yell_angrily(words)
+#    words + "!!!" + " :("
+#  end
+#
+#  def self.yell_happily(words)
+#    words + "!!!" + " :)"
+#  end
+#end 
+#
+#p Shout2.yell_angrily("hey")
+#p Shout2.yell_happily("hey")
+
 module Shout2
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    words.upcase + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
-    words + "!!!" + " :)"
+  def yell_drunkenly(words)
+    words.swapcase + "!*" 
   end
 end 
 
-p Shout2.yell_angrily("hey")
-p Shout2.yell_happily("hey")
+class Old_man
+  include Shout2
+end
+
+class Punk_singer
+  include Shout2
+end
+
+man = Old_man.new
+p man.yell_angrily("get off my lawn")
+
+punk = Punk_singer.new
+p punk.yell_drunkenly("Bla Bla bBa")
