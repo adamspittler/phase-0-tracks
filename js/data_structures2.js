a62194e332fcf2cca6cf9acb776a045843ae8f0b
adamspittler <spittler@gmail.com>
2016-05-25 12:08:37 -0400

    Add release 0

---
+++ js/data_structures2.js
@@ -0,0 +1,7 @@
+var colors = ["blue", "white", "black", "green"];
+var names = ["bill", "charlie", "mary", "darleen"];
+
+names.push("chuck");
+colors.push("yellow")
+console.log(names);
+console.log(colors);
