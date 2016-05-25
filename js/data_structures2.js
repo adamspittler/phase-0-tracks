5b83f4d41df313915da671ebd1dced7fc30d4172
adamspittler <spittler@gmail.com>
2016-05-25 12:21:23 -0400

    Add release 1.

--- js/data_structures2.js
+++ js/data_structures2.js
@@ -5,3 +5,10 @@ names.push("chuck");
 colors.push("yellow")
 console.log(names);
 console.log(colors);
+
+var horses = {};
+
+for (var i = 0; i < colors.length; i++) {
+  horses[(names[i])] = (colors[i]);
+}
+console.log(horses);
