b3c73384ff3c48404561490a4e00007fb9285182
adamspittler <spittler@gmail.com>
2016-05-26 15:37:49 -0400

    Add release 2.

--- js/algos2.js
+++ js/algos2.js
@@ -1,8 +1,27 @@
-// FUNCTION: findLongest, Takes an array.
-// Defines an empty array for the length of the strings in the array.
+// FUNCTION: findLongest, takes an array.
+// Defines an empty array strLengthArray
 // Runs a loop to add the string lengths to the second array.
-// Finds the index of the highest value in the string length array.
-// Returns the string at the same index in the original array.
+// Finds the index of the highest value in the stringLengthArray.
+// Returns the longest string: the string at the same index in the inputted array.
+
+// FUNCTION: checkMatch, takes two objects as arguments.
+// Sets variable isMatch value to begin as false.
+// Creates variables out of the keys in the objects.
+// Checks if the keys match with the key variables.
+// Checks if the values match using the key variables to access the values.
+// Returns isMatch boolean value.
+
+// FUNCTION: randomString, no input.
+// Defines variable stringLength which is a random number between 1 and 10.
+// Defines empty array randomChars.
+// Defines variable alph which is a string of the lower case alphabet.
+// Adds the number that is stringLength of randomly selected characters from the alphabet string to the randomChars string.
+// Returns randomChars string.
+
+// FUNCTION: stringNumber, takes a number as argument.
+// Defines empty array called randArray.
+// Calls randomString function and adds results to randArray the user inputted number of times.
+// Prints and returns randArray
 
 function findLongest(ary) {
   var strLengthArray = [];
@@ -12,15 +31,6 @@ function findLongest(ary) {
   var largest = Math.max.apply(Math, strLengthArray);
   return (ary[(strLengthArray.indexOf(largest))]);
 }
-// DRIVERCODE:
-// console.log(findLongest(["long phrase","longest phrase","longer phrase"]));
-
-// FUNCTION: checkMatch: takes two objects as arguments.
-// Sets variable isMatch value to begin as false.
-// Creates variables out of the keys in the objects.
-// Checks if the keys match with the key variables.
-// Checks if the values match using the key variables to access the values.
-// Returns match boolean value.
 
 function checkMatch(obj1, obj2) {
   var isMatch = false;
@@ -35,5 +45,27 @@ function checkMatch(obj1, obj2) {
   }
   return isMatch
 }
+
+function randomString() {
+  var stringLength = Math.floor( Math.random() * 9 +1 );
+    var randomChars = "";
+    var alph = "abcdefghijklmnopqrstuvwxyz";
+  for( var i=0; i < stringLength; i++ )
+        randomChars += alph.charAt(Math.floor(Math.random() * alph.length));
+  return randomChars;
+}
+
+function stringNumber(chosenNumber) {
+  var randArray = [];
+  for (var i = 0; i < chosenNumber; i++) { 
+    randArray.push((randomString()));
+  }
+  console.log(randArray)
+  return randArray;
+}
+
+// DRIVERCODE:
+console.log(findLongest(["long phrase","longest phrase","longer phrase"]));
 console.log(checkMatch({name: "Steven", age: 53}, {name: "Tamir", age: 54})); 
 console.log(checkMatch({name: "Steven", age: 53}, {name: "Steven", age: 54})); 
+console.log(findLongest((stringNumber(5))));
