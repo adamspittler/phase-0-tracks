0b0c84a254bab0c62a9c9408e91b3e4b032db600
adamspittler <spittler@gmail.com>
2016-06-07 15:25:49 -0400

    Add release 2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,3 +1,12 @@
-console.log("check");
-var background = document.getElementById("nav_box");
-background.style.border = "4px solid black"
+
+
+
+function changeSize(event) {
+  console.log(event);
+  event.target.style.backgroundColor = "white" 
+  event.target.style.float = "right" 
+}
+var background = document.getElementById("main_content");
+background.addEventListener("click", changeSize);
+
+
